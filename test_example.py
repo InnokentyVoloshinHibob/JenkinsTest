61b950584df751c30b22dc0b905ff5238a053171
InnokentyVoloshinHibob <innokenty.voloshin@hibob.io>
2022-06-21 09:45:32 +0300

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -5,15 +5,12 @@ class Example(BaseCase):
 
     @staticmethod
     def test_example1():
-        if 2+2 != 4:
-            assert False
+        assert 2+2 != 4
 
     @staticmethod
     def test_example2():
-        if 2 * 2 != 4:
-            assert False
+        assert 2 * 2 != 4
 
     @staticmethod
     def test_example3():
-        if 2 / 2 != 1:
-            assert False
+        assert 2 / 2 != 1
